f2fb4299fa359f20e8634d922e98f400b6ed5c89
Masatoshi Teruya <mah0x211@gmail.com>
2014-05-07 14:04:29 +0900

    change: update.sh has been changed.

--- bin/update.sh
+++ bin/update.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-if ! [ -d "$1" ]; then
-    echo "repository $1 not found."
-    exit 1;
-fi
-
 set -e
 set -x
 
-
+CMD_DIR=$(cd "$(dirname $0)"; pwd)
 ROCKS_ADMIN=`which luarocks-admin`
 LUA=`which lua`
-CMD_DIR=$(cd "$(dirname $0)"; pwd)
 MAKEZIP=$CMD_DIR/makeZip.lua
 
+if ! [ -d "$1" ]; then
+    echo "repository $1 not found."
+    exit 1;
+elif ! [ -f "$2" ]; then
+    echo "rockspec $2 not found."
+    exit 1;
+fi
+
 REPO=$(cd $1; pwd)
-echo "update repository $REPO"
+SPEC=$(basename $2)
+SPEC_FILE="$(cd $(dirname $2); pwd)/$SPEC"
+
 cd $REPO
+cp $SPEC_FILE ./ 
+
+echo "update repository $REPO"
 # create manifest
 $ROCKS_ADMIN make_manifest .
 # zip manifest files
 $LUA $MAKEZIP
 
-echo "DONE"
+git checkout master
+git add index.html manifest* $SPEC
+git commit -m "add $SPEC"
